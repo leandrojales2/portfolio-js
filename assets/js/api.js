c9d83b969b374390248eac154acf2b060bdade8c
leandrojales2 <desenvolvedorjales@yahoo.com>
2024-06-04 21:22:45 -0300

    Atualizações de pastas e criação de dinamismo nas informações

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,6 +2,6 @@
 async function fetchProfileData() {
     const url = 'https://raw.githubusercontent.com/leandrojales2/portfolio-js/main/data/profile.json';
     const fetching = await fetch(url)
-    return await fetching.json
+    return await fetching.json('')
 }
 
